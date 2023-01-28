d85819d722a150e41795e77fbfbc7b3f3936f163
Daisy <ysiad926@gmail.com>
2023-01-28 11:08:58 +0000

    Class GildedRose second test

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,8 +2,15 @@ require 'gilded_rose'
 require 'item'
 
 RSpec.describe GildedRose do
-  it "" do
+  it "should show [] if no item added" do
     gilded_rose = GildedRose.new
     expect(gilded_rose.show_items).to eq([])  
+  end
+
+  it "should show item1 if item1 added" do
+    item = Item.new('item1',10,20)
+    gilded_rose = GildedRose.new
+    gilded_rose.add(item)
+    expect(gilded_rose.show_items).to eq([item])  
   end
 end
